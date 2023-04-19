fb5b0283acdeca27cea88ae8988b248cf3048579
Dan Baron <129026313+blackhalomotorcycleclub@users.noreply.github.com>
2023-04-19 07:44:58 -0500

    Added save_brain and load_brain methods
    - Added `collect_training_data` function to collect training data from an IRC channel.
    - Added `save_brain` and `load_brain` methods to the `DeepLearningChatbot` class to save and load the state of the tokenizer and model to and from a file named `$botname.brain`.
    - The `train` method now automatically saves the state of the tokenizer and model to a file when training is complete.
    - When a new instance of the `DeepLearningChatbot` class is created with the same `nickname`, it automatically loads the saved state of the tokenizer and model from the file and continues where it left off.

--- clive.py
+++ clive.py
@@ -6,17 +6,20 @@ from keras.layers import LSTM, Dense
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 import numpy as np
+import pickle
 
 class DeepLearningChatbot(SingleServerIRCBot):
     def __init__(self, server, channel, nickname):
         super().__init__([(server, 6667)], nickname, nickname)
         self.channel = channel
+        self.nickname = nickname
         self.tokenizer = Tokenizer()
         self.max_len = 20
         self.model = Sequential()
         self.model.add(LSTM(128, input_shape=(self.max_len, 1)))
         self.model.add(Dense(1, activation='sigmoid'))
         self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        self.load_brain()
 
     def on_welcome(self, connection, event):
         connection.join(self.channel)
@@ -31,6 +34,7 @@ class DeepLearningChatbot(SingleServerIRCBot):
         inputs = self._preprocess(inputs)
         outputs = np.array(outputs)
         self.model.fit(inputs, outputs, epochs=epochs)
+        self.save_brain()
 
     def respond(self, message):
         message = self._preprocess([message])
@@ -42,11 +46,39 @@ class DeepLearningChatbot(SingleServerIRCBot):
         sequences = self.tokenizer.texts_to_sequences(messages)
         return pad_sequences(sequences, maxlen=self.max_len)
 
+    def save_brain(self):
+        filename = f"{self.nickname}.brain"
+        with open(filename, 'wb') as f:
+            pickle.dump((self.tokenizer.word_index, self.model.get_weights()), f)
+
+    def load_brain(self):
+        filename = f"{self.nickname}.brain"
+        try:
+            with open(filename, 'rb') as f:
+                word_index, weights = pickle.load(f)
+            self.tokenizer.word_index = word_index
+            self.model.set_weights(weights)
+        except FileNotFoundError:
+            pass
+
+def collect_training_data(server, channel, nickname):
+    inputs = []
+    outputs = []
+    bot = SingleServerIRCBot([(server, 6667)], nickname, nickname)
+    def on_pubmsg(connection,event):
+        message = event.arguments[0]
+        inputs.append(message)
+        response = input(f"Enter response for '{message}': ")
+        outputs.append(response)
+    bot.on_pubmsg = on_pubmsg
+    bot.start()
+    return inputs, outputs
+
 def setup():
     server = input('Enter IRC server: ')
     channel = input('Enter IRC channel: ')
     nickname = input('Enter chatbot nickname: ')
-    chatbot = DeepLearningChatbot(server, channel, nickname)
+    chatbot = DeepLearningChatbot(server, channel,nickname)
     chatbot.start()
 
 setup()
